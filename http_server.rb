2910ec8edbd195bdb6911cb8fd90bdde0c044522
gikundi <denisgikundi05@gmail.com>
2023-11-11 14:24:43 +0300

    added comments to make the code more readable

--- http_server.rb
+++ http_server.rb
@@ -1,20 +1,31 @@
+# Require the 'socket' library to work with TCP sockets
 require 'socket'
 
+# Create a TCP server that listens on all available interfaces (0.0.0.0) and port 8080
 server  = TCPServer.new('0.0.0.0', 80)
 
+
+# Infinite loop to continuously accept incoming connections
 loop {
+
+    # Accept a client connection and read the request
   client  = server.accept
   request = client.readpartial(2048)
   
+  # Parse the first line of the HTTP request to extract method, path, and version
   method, path, version = request.lines[0].split
 
+   # Print the request information to the console
   puts "#{method} #{path} #{version}"
 
+  # If the path is /healthcheck, return a 200 OK response with the body "OK"
   if path == "/healthcheck"
     client.write("OK")
+  # If the path is /hello, return a 200 OK response with the body "Well, hello there!"
   else
     client.write("Well, hello there!")
   end
 
+  # Close the client connection
   client.close
 }
